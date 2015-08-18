e42f9e7addd3b1dc9571054fb471dc636b3ee407
Matt Johnson <matt.johnson@classyllama.com>
2015-08-18 12:28:48 -0500

    Badge initially overriding template

---
+++ module/Block/Product/Badge.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace MagentoEse\ProductBadge\Block\Product;
+
+class Badge extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Template image with html frame border
+     *
+     * @var string
+     */
+    protected $_templateBadge = 'MagentoEse_ProductBadge::product/image.phtml';
+
+    /**
+     * @var \MagentoEse\ProductBadge\Model\Badge
+     */
+    protected $_badge;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \MagentoEse\ProductBadge\Model\Badge $badgeView
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \MagentoEse\ProductBadge\Model\Badge $badge,
+        array $data = []
+    ) {
+        $this->_badge = $badge;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Initialize model
+     *
+     * @param \Magento\Catalog\Block\Product\Image $return
+     * @return \MagentoEse\ProductBadge\Block\Product\Badge
+     */
+    public function init($product)
+    {
+        $this->_badge->init($product);
+
+        if (null === $this->getTemplate()) {
+            $this->setTemplate($this->_templateBadge);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Getter Badge Label
+     *
+     * @return string
+     */
+    public function getBadgeLabel()
+    {
+        return $this->_badge->getLabel();
+    }
+
+    /**
+     * Getter Badge Label
+     *
+     * @return string
+     */
+    public function getBadgeClass()
+    {
+        return $this->_badge->getClassName();
+    }
+
+    /**
+     * Returns status if the product has a badge or not
+     *
+     * @return boolean
+     */
+    public function hasBadge()
+    {
+        return $this->_badge->hasBadge();
+    }
+
+}
---
+++ module/Model/Badge.php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * Magento ESE product badge model
+ */
+namespace MagentoEse\ProductBadge\Model;
+
+class Badge
+{
+    /**
+     * @var []
+     */
+    private $_badgeList;
+
+    /**
+     * @var []
+     */
+    private $_badgeListDefaults;
+
+    /**
+     * Initialize block
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @return $this
+     */
+    public function init($product)
+    {
+        $this->_badgeList = $product->getAttributeText('badge');
+        $this->_badgeListDefaults = $this->getDefaultAttributeText($product, 'badge');
+        return $this;
+    }
+
+    /**
+     * Returns the default attribute text value/s for a product
+     *
+     * @param \Magento\Catalog\Model\Product $product
+     * @param string $attributeCode
+     * @return string
+     */
+    private function getDefaultAttributeText($product, $attributeCode)
+    {
+        $value = $product->getData($attributeCode);
+
+        // Reference:
+        //      Magento\Eav\Model\Entity\Attribute\Source
+        //          getOptionText()
+        $isMultiple = false;
+        if (strpos($value, ',')) {
+            $isMultiple = true;
+            $value = explode(',', $value);
+        }
+
+        // get the default (Admin store_id=0) values for the options
+        // Reference:
+        //      Magento\Catalog\Model\Product
+        //          getAttributeText()
+        //      Magento\Eav\Model\Entity\Attribute\Source
+        //          getAllOptions()
+        //          getOptionText()
+        $options = $product->getResource()->getAttribute($attributeCode)->getSource()->getAllOptions(false, true);
+
+        if ($isMultiple) {
+            $values = [];
+            foreach ($options as $item) {
+                if (in_array($item['value'], $value)) {
+                    $values[] = $item['label'];
+                }
+            }
+            return $values;
+        }
+
+        foreach ($options as $item) {
+            if ($item['value'] == $value) {
+                return $item['label'];
+            }
+        }
+    }
+
+    /**
+     * Return product badge formatted class name
+     *
+     * @return string
+     */
+    public function getClassName()
+    {
+        if ($this->hasBadge()) {
+            preg_match("/.+\(([\w-]+)\)+/", $this->getSingleBadge($this->_badgeListDefaults), $regexArrayResults);
+            if (empty($regexArrayResults[1])) {
+                $className = strtolower(preg_replace("/\W/", "-", $this->getSingleBadge($this->_badgeListDefaults)));
+            } else {
+                $className = strtolower(preg_replace("/\W/", "-", $regexArrayResults[1]));
+            }
+            return $className;
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * Return product badge label
+     *
+     * @return string
+     */
+    public function getLabel()
+    {
+        if ($this->hasBadge()) {
+            return $this->getSingleBadge();
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * Returns status if the product has a badge or not
+     *
+     * @return boolean
+     */
+    public function hasBadge()
+    {
+        if (empty($this->_badgeList))
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
+    }
+
+    /**
+     * Return a single badge from the list of badges
+     *
+     * @param array $badgeList
+     * @return string
+     */
+    private function getSingleBadge($badgeList = null)
+    {
+        if ($badgeList === null) {
+            $badgeList = $this->_badgeList;
+        }
+
+        if (is_array($badgeList))
+        {
+            return $badgeList[0];
+        } else {
+            return $badgeList;
+        }
+    }
+}
