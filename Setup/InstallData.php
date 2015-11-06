6de57dc767816bc9a2678deab9170d86dd28fef0
Matt Johnson <matt.johnson@classyllama.com>
2015-11-06 12:36:24 -0600

    Added database transaction wrapper to setup scripts

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -36,47 +36,58 @@ class InstallData implements InstallDataInterface
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        /** @var EavSetup $eavSetup */
-        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $connection = $setup->getConnection();
+        try {
+            $connection->beginTransaction();
 
-        /**
-         * Add attributes to the eav/attribute
-         */
+            /** @var EavSetup $eavSetup */
+            $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
 
-        $eavSetup->addAttribute(
-            \Magento\Catalog\Model\Product::ENTITY,
-            'badge',
-            [
-                'type' => 'varchar',
-                'label' => 'Product Badge',
-                'input' => 'multiselect',
-                'required' => false,
-                'backend' => 'Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend',
-                'global' => \Magento\Catalog\Model\ResourceModel\Eav\Attribute::SCOPE_GLOBAL,
-                'visible' => true,
-                'user_defined' => false,
-                'searchable' => false,
-                'filterable' => true,
-                'filterable_in_search' => true,
-                'comparable' => false,
-                'visible_on_front' => false,
-                'unique' => false,
-                'used_in_product_listing' => true,
-                'group' => 'Product Details',
-                'is_used_in_grid' => true,
-                'is_visible_in_grid' => true,
-                'is_filterable_in_grid' => true,
-                'option' => [
-                    'values' => [
-                        'Sale',
-                        'New',
-                        'In-Store Pickup',
-                        'Best Seller',
-                        'Most Shared',
-                        'Custom Example (Pink)'
+            /**
+             * Add attributes to the eav/attribute
+             */
+
+            $eavSetup->addAttribute(
+                \Magento\Catalog\Model\Product::ENTITY,
+                'badge',
+                [
+                    'type' => 'varchar',
+                    'label' => 'Product Badge',
+                    'input' => 'multiselect',
+                    'required' => false,
+                    'backend' => 'Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend',
+                    'global' => \Magento\Catalog\Model\ResourceModel\Eav\Attribute::SCOPE_GLOBAL,
+                    'visible' => true,
+                    'user_defined' => false,
+                    'searchable' => false,
+                    'filterable' => true,
+                    'filterable_in_search' => true,
+                    'comparable' => false,
+                    'visible_on_front' => false,
+                    'unique' => false,
+                    'used_in_product_listing' => true,
+                    'group' => 'Product Details',
+                    'is_used_in_grid' => true,
+                    'is_visible_in_grid' => true,
+                    'is_filterable_in_grid' => true,
+                    'option' => [
+                        'values' => [
+                            'Sale',
+                            'New',
+                            'In-Store Pickup',
+                            'Best Seller',
+                            'Most Shared',
+                            'Custom Example (Pink)'
+                        ]
                     ]
                 ]
-            ]
-        );
+            );
+            $connection->commit();
+        } catch (\Exception $e) {
+
+            // If an error occured rollback the database changes as if they never happened
+            $connection->rollback();
+            throw $e;
+        }
     }
 }
